5976b26501ce73cc727d056f3f985e71d0073d17
usigna <bukazaokna@gmail.com>
2021-09-30 21:20:02 +0200

    Add gallery

--- js/app.js
+++ js/app.js
@@ -100,12 +100,42 @@ function copiedToClipboard() {
   }
 }
 
+function useGallery() {
+  function handler(e) {
+    e.preventDefault();
+    largeimage.setAttribute("src", this.getAttribute("href"));
+    largeimage.setAttribute("alt", this.querySelector("img").getAttribute("alt"));
+    largeimage.animate([
+      { opacity: '0'},
+      { opacity: '1'}
+    ], {
+      duration: 500
+    });
+  }
+
+  const gallery = document.querySelector('.gallery');
+  const largeimagecontainer = document.querySelector('.fullimagecontainer');
+  const links = gallery.getElementsByTagName('a');
+  const largeimage = document.createElement("img");
+
+  largeimage.setAttribute("id", "fullimage");
+  largeimagecontainer.appendChild(largeimage);
+      
+  for (var i=0; i<links.length; i++) {
+    links[i].onclick = handler;
+  }
+      
+  links[0].focus();
+  links[0].click();
+}
+
 const init = function () {
   showHamburgerMenu();
-  changeLanguage();
+  // changeLanguage();
   useParallax();
   setImageMaxSize();
   copiedToClipboard();
+  useGallery();
 };
 
 document.addEventListener('DOMContentLoaded', init);
